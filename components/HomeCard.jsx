173c5163c9eaac732d98b2c9ee7efd407b5765e5
i-spin <me@i-sp.in>
2022-07-06 19:27:53 -0700

    add social links on main page

--- components/HomeCard.jsx
+++ components/HomeCard.jsx
@@ -1,17 +1,35 @@
-import { Card, Grid, Image, Text } from "@geist-ui/core";
+import { Card, Grid, Image, Link, Text } from "@geist-ui/core";
+import { Twitter } from "@geist-ui/icons";
+import styles from "../styles/HomeCard.module.css";
+import SocialTag from "./SocialTag";
 
 export default function HomeCard(props) {
   const { profile } = props;
-  const { title, name, image } = profile;
 
   return (
-    <Card style={{height: image.dimentions.height ?? "auto"}}>
-      <Grid.Container gap={3} justify="space-evenly">
+    <Card style={{ height: profile.image.dimentions.height }}>
+      <Grid.Container gap={5} justify="space-between">
         <Grid md={12}>
-          <Image draggable={false} src={image.path} alt={`${name}'s profile`} width={image.dimentions.width ?? "auto"} height={image.dimentions.height ?? "auto"} />
+          <Image
+            draggable={false}
+            src={profile.image.path}
+            alt={`${profile.name}'s profile`}
+            width={profile.image.dimentions.width}
+            height={profile.image.dimentions.height}
+          />
         </Grid>
-        <Grid lg={12}>
-          <Text h1>{title}</Text>
+        <Grid md={12}>
+          <div className={styles["profile-details"]}>
+            <Text h1>{profile.title}</Text>
+            <div className={styles["profile-contacts"]}>
+              <Text h3>Contacts:</Text>
+              <div className={styles["profile-contacts-links"]}>
+                {profile.contacts.map((contact) => (
+                  <SocialTag platform={contact.platform} iconSize={20} user={contact.value} />
+                ))}
+              </div>
+            </div>
+          </div>
         </Grid>
       </Grid.Container>
     </Card>
---
+++ components/SocialTag.jsx
@@ -0,0 +1,44 @@
+import { FaDiscord, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import { Link, useClipboard, useToasts } from "@geist-ui/core";
+
+export default function SocialTag(props) {
+  const { platform, user, iconSize } = props;
+  const { setToast } = useToasts();
+  const { copy } = useClipboard();
+
+  switch (platform) {
+    case "Discord":
+      return (
+        <Link underline href='#' onClick={() => {
+          copy(user);
+          setToast({text: 'Copied to clipboard.', delay: 2000});
+        }}>
+          <FaDiscord size={iconSize} style={{ margin: "0.5rem" }} />
+          {user}
+        </Link>
+      );
+    case "GitHub":
+      return (
+        <Link icon underline href={`https://github.com/${user}`}>
+          <FaGithub size={iconSize} style={{ margin: "0.5rem" }} />
+          {user}
+        </Link>
+      );
+    case "Twitter":
+      return (
+        <Link icon underline href={`https://twitter.com/${user}`}>
+          <FaTwitter size={iconSize} style={{ margin: "0.5rem" }} />
+          {user}
+        </Link>
+      );
+    case "Instagram":
+      return (
+        <Link icon underline href={`https://instagram.com/${user}`}>
+          <FaInstagram size={iconSize} style={{ margin: "0.5rem" }} />
+          {user}
+        </Link>
+      );
+    default:
+      return;
+  }
+}
